8ba063f3137132b40e56f65762aa6e9077651cfa
Denrei Keith <denzkei12@gmail.com>
2022-09-10 17:37:32 +0800

    Initial commit

---
+++ src/main/java/com/commerce/App.java
@@ -0,0 +1,13 @@
+package com.commerce;
+
+// Things to add
+// txtmark - renders md syntax to html (good for formatting)
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
