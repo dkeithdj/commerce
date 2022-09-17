865edf2a097b86ae2ec9d64cfcca18e3add82290
Denrei Keith <denzkei12@gmail.com>
2022-09-17 15:26:40 +0800

    feat: getters and setters

--- src/main/java/com/proj/commerce/models/Product.java
+++ src/main/java/com/proj/commerce/models/Product.java
@@ -1,15 +1,109 @@
 package com.proj.commerce.models;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
 @Table
 public class Product {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
+
   private String title;
+
   private String description;
-  private User user;
+
+  private double price;
+
+  // @JoinColumn(name = "user_id", referencedColumnName = "id", foreignKey =
+  // @ForeignKey(name = "user_product_fk"))
+  @ManyToOne
+  @JoinColumn(name = "user_info_id")
+  private UserInfo user_info;
+
   private String image;
+
+  private LocalDateTime date;
+
+  private int stocks;
+
+  public Product() {
+  }
+
+  public Product(String title, String description, double price, String image, int stocks) {
+    this.title = title;
+    this.description = description;
+    this.price = price;
+    this.image = image;
+    this.date = LocalDateTime.now();
+    this.stocks = stocks;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public double getPrice() {
+    return price;
+  }
+
+  public void setPrice(double price) {
+    this.price = price;
+  }
+
+  public UserInfo getUser() {
+    return user_info;
+  }
+
+  public void setUser(UserInfo user) {
+    this.user_info = user;
+  }
+
+  public String getImage() {
+    return image;
+  }
+
+  public void setImage(String image) {
+    this.image = image;
+  }
+
+  public LocalDateTime getDate() {
+    return date;
+  }
+
+  public int getStocks() {
+    return stocks;
+  }
+
+  public void setStocks(int stocks) {
+    this.stocks = stocks;
+  }
 
 }
