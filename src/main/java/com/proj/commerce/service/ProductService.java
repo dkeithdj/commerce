02eecd9d37eae10128a29e7a402323977d47c6f0
Denrei Keith <denzkei12@gmail.com>
2022-09-18 22:48:19 +0800

    feat: added services for table Product

---
+++ src/main/java/com/proj/commerce/service/ProductService.java
@@ -0,0 +1,17 @@
+package com.proj.commerce.service;
+
+import java.util.List;
+
+import com.proj.commerce.models.Product;
+
+public interface ProductService {
+  Product saveProduct(Product product);
+
+  List<Product> fetchProductList();
+
+  List<Product> fetchProductListByClient(Long clientId);
+
+  Product updateProduct(Product product, Long productId);
+
+  void deleteProductById(Long productId);
+}
---
+++ src/main/java/com/proj/commerce/service/ProudctServiceImpl.java
@@ -0,0 +1,55 @@
+package com.proj.commerce.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.proj.commerce.models.Product;
+import com.proj.commerce.repositories.ProductRepository;
+
+@Transactional
+@Service
+public class ProudctServiceImpl implements ProductService {
+  @Autowired
+  private ProductRepository productRepository;
+
+  @Override
+  public Product saveProduct(Product product) {
+    // TODO Auto-generated method stub
+    return productRepository.save(product);
+  }
+
+  @Override
+  public List<Product> fetchProductList() {
+    // TODO Auto-generated method stub
+    return (List<Product>) productRepository.findAll();
+  }
+
+  @Override
+  public Product updateProduct(Product product, Long productId) {
+    Product productDB = productRepository.findById(productId).get();
+
+    productDB.setTitle(product.getTitle());
+    productDB.setDescription(product.getDescription());
+    productDB.setImage(product.getImage());
+    productDB.setPrice(product.getPrice());
+    productDB.setStocks(product.getStocks());
+    productDB.setClient(product.getClient());
+
+    return productRepository.save(productDB);
+  }
+
+  @Override
+  public void deleteProductById(Long productId) {
+    productRepository.deleteById(productId);
+
+  }
+
+  @Override
+  public List<Product> fetchProductListByClient(Long userId) {
+    return productRepository.findByClientId(userId);
+  }
+
+}
