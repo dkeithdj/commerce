ed6bb3f4480a34a8fb26bfa5d055b939dd653870
Denrei Keith <denzkei12@gmail.com>
2022-09-27 01:17:13 +0800

    feat: added methods for Entity access

--- src/main/java/com/proj/commerce/repositories/OrderRepository.java
+++ src/main/java/com/proj/commerce/repositories/OrderRepository.java
@@ -1,13 +1,24 @@
 package com.proj.commerce.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.proj.commerce.models.Order;
+import com.proj.commerce.models.Product;
 
 @Repository
 @Transactional
 public interface OrderRepository extends JpaRepository<Order, Long> {
+  List<Order> findByClientId(Long client_id);
+
+  Order findByClientIdAndProductsId(Long client_id, Long product_id);
+
+  // @Query(value = "SELECT * FROM ORDER_PRODUCT")
+  // List<Order> getOrders();
+  // Order findByProductsId(Long id);
 
 }
