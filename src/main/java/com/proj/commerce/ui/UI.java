37d4d24bd16b30bd3aee1bcddfd8fa2e49579205
Denrei Keith <denzkei12@gmail.com>
2022-09-27 01:15:08 +0800

    feat: made colors final

--- src/main/java/com/proj/commerce/ui/UI.java
+++ src/main/java/com/proj/commerce/ui/UI.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -18,23 +20,22 @@ import javax.swing.JPasswordField;
 import javax.swing.JSpinner;
 import javax.swing.JTextField;
 import javax.swing.SpinnerNumberModel;
-import javax.swing.plaf.basic.BasicArrowButton;
 
 public class UI {
-  public final Color bg0 = new Color(36, 40, 59);
-  public final Color bg1 = new Color(65, 72, 104);
-  public final Color bg2 = new Color(26, 27, 38);
-  public final Color purple0 = new Color(187, 154, 247);
-  public final Color purple1 = new Color(122, 162, 247);
-  public final Color blue0 = new Color(125, 207, 255);
-  public final Color blue1 = new Color(42, 195, 222);
-  public final Color green0 = new Color(115, 218, 202);
-  public final Color green1 = new Color(158, 206, 106);
-  public final Color orange0 = new Color(224, 175, 104);
-  public final Color orange1 = new Color(255, 158, 100);
-  public final Color red = new Color(247, 118, 142);
-  public final Color fg0 = new Color(169, 177, 214);
-  public final Color fg1 = new Color(192, 202, 245);
+  public final Color BG0 = new Color(36, 40, 59);
+  public final Color BG1 = new Color(65, 72, 104);
+  public final Color BG2 = new Color(26, 27, 38);
+  public final Color PURPLE0 = new Color(187, 154, 247);
+  public final Color PURPLE1 = new Color(122, 162, 247);
+  public final Color BLUE0 = new Color(125, 207, 255);
+  public final Color BLUE1 = new Color(42, 195, 222);
+  public final Color GREEN0 = new Color(115, 218, 202);
+  public final Color GREEN1 = new Color(158, 206, 106);
+  public final Color ORANGE0 = new Color(224, 175, 104);
+  public final Color ORANGE1 = new Color(255, 158, 100);
+  public final Color RED = new Color(247, 118, 142);
+  public final Color FG0 = new Color(169, 177, 214);
+  public final Color FG1 = new Color(192, 202, 245);
 
   public final String styleSheet() {
     Path stylesLoc = Path.of("./static/styles.css");
@@ -50,15 +51,15 @@ public class UI {
 
   public class CustomPanel extends JPanel {
     public CustomPanel() {
-      setBackground(bg0);
+      setBackground(BG0);
     }
 
   }
 
   public class CustomButton extends JButton {
     public CustomButton() {
-      setBackground(fg0);
-      setForeground(bg0);
+      setBackground(FG0);
+      setForeground(BG0);
       setFocusPainted(false);
       setFont(new Font("Tahoma", Font.PLAIN, 12));
 
@@ -66,8 +67,8 @@ public class UI {
 
     public CustomButton(String text) {
       setText(text);
-      setBackground(fg0);
-      setForeground(bg0);
+      setBackground(FG0);
+      setForeground(BG0);
       setFocusPainted(false);
       setFont(new Font("Tahoma", Font.PLAIN, 12));
 
@@ -87,7 +88,7 @@ public class UI {
 
     public CustomLabel(String text) {
       setText(text);
-      setForeground(fg1);
+      setForeground(FG1);
       setFont(new Font("Tahoma", Font.PLAIN, 14));
     }
 
@@ -96,10 +97,10 @@ public class UI {
   public class CustomTextField extends JTextField {
     public CustomTextField(int columns) {
       setColumns(columns);
-      setBackground(new UI().bg1);
-      setForeground(new UI().fg1);
-      setCaretColor(new UI().blue0);
-      setBorder(BorderFactory.createLineBorder(bg0, 0));
+      setBackground(BG1);
+      setForeground(FG1);
+      setCaretColor(BLUE0);
+      setBorder(BorderFactory.createLineBorder(BG0, 0));
       setFont(new Font("Tahoma", Font.PLAIN, 14));
     }
   }
@@ -107,15 +108,16 @@ public class UI {
   public class CustomPasswordField extends JPasswordField {
     public CustomPasswordField(int columns) {
       setColumns(columns);
-      setBackground(new UI().bg1);
-      setForeground(new UI().fg1);
-      setCaretColor(new UI().blue0);
-      setBorder(BorderFactory.createLineBorder(bg0, 0));
+      setBackground(BG1);
+      setForeground(FG1);
+      setCaretColor(BLUE0);
+      setBorder(BorderFactory.createLineBorder(BG0, 0));
       setFont(new Font("Tahoma", Font.PLAIN, 14));
     }
   }
 
   public class CustomSpinner extends JSpinner {
+
     public CustomSpinner(SpinnerNumberModel model) {
       setModel(model);
       JComponent spinnerEditor = getEditor();
@@ -128,18 +130,32 @@ public class UI {
       for (int i = 0; i < n; i++) {
         Component c = spinnerEditor.getComponent(i);
         if (c instanceof JFormattedTextField) {
-          c.setBackground(new UI().bg1);
-          c.setForeground(new UI().fg1);
+          c.setBackground(BG1);
+          c.setForeground(FG1);
 
         }
         if (c instanceof JButton) {
-          c.setBackground(new UI().bg1);
-          c.setForeground(new UI().fg1);
+          c.setBackground(BG1);
+          c.setForeground(FG1);
 
         }
       }
     }
 
+  }
+
+  public ActionListener redirectListener(JPanel mainPnl, JPanel bodyPnl, JPanel pnl) {
+    ActionListener al = new ActionListener() {
+
+      @Override
+      public void actionPerformed(ActionEvent e) {
+        bodyPnl.removeAll();
+        bodyPnl.add(pnl);
+        mainPnl.revalidate();
+        mainPnl.repaint();
+      }
+    };
+    return al;
   }
 
 }
